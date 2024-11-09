0a663b6c07d848401470fecf3bc7be604ee1f16e
javicod92 <javi.javier.jc@gmail.com>
2024-11-09 14:44:43 -0300

    Adding register function

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,90 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectMongoDB } from "@/lib/mongodb";
+import { messages } from "@/utils/messages";
+import { isValidEmail } from "@/utils/isValidEmail";
+import User, { IUserSchema } from "@/models/User";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export async function POST(request: NextRequest) {
+  try {
+    await connectMongoDB();
+    const body = await request.json();
+    const { email, password, confirmPassword } = body;
+
+    //function to validate all fields
+    if (!email || !password || !confirmPassword) {
+      return NextResponse.json(
+        { message: messages.error.needProps },
+        { status: 400 }
+      );
+    }
+
+    //function to validate the email entered by the user
+    if (!isValidEmail(email)) {
+      return NextResponse.json(
+        { message: messages.error.emailNotValid },
+        { status: 400 }
+      );
+    }
+
+    //function to verify if password === confirmPassword
+    if (password !== confirmPassword) {
+      return NextResponse.json(
+        { message: messages.error.passwordsNotMatch },
+        { status: 400 }
+      );
+    }
+
+    //Before to save a new user, we need to verify if the email entered exists or not in the database
+    const userFind = await User.findOne({ email });
+    if (userFind) {
+      return NextResponse.json(
+        { message: messages.error.emailExist },
+        { status: 200 }
+      );
+    }
+
+    //function to encrypt the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //function to generate a new user
+    const newUser: IUserSchema = new User({
+      email,
+      password: hashedPassword,
+    });
+
+    //@ts-ignore
+    const { password: userPass, ...rest } = newUser._doc;
+
+    await newUser.save();
+
+    const token = jwt.sign({ data: rest }, "secreto", {
+      expiresIn: 86400,
+    });
+
+    const response = NextResponse.json(
+      {
+        newUser: rest,
+        message: messages.success.userCreated,
+      },
+      {
+        status: 200,
+      }
+    );
+
+    response.cookies.set("auth_cookie", token, {
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 86400,
+      path: "/",
+    });
+
+    return response;
+  } catch (error) {
+    return NextResponse.json(
+      { message: messages.error.default, error },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/utils/messages.ts
@@ -0,0 +1,12 @@
+export const messages = {
+  error: {
+    needProps: "Falto enviar algún campo",
+    emailNotValid: "El email no es valido",
+    passwordsNotMatch: "Las claves ingresadas no coinciden",
+    emailExist: "El email ingresado ya existe",
+    default: "Ocurrio un error",
+  },
+  success: {
+    userCreated: "Usuario creado con éxito",
+  },
+};
