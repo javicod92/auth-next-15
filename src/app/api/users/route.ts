a9fc0a2b3d5f48a0d3d2e7267b9f16f521914781
javicod92 <javi.javier.jc@gmail.com>
2024-11-11 02:05:39 -0300

    Adding show all users function

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,1 +1,18 @@
-export function GET() {}
+import { NextRequest, NextResponse } from "next/server";
+import { connectMongoDB } from "@/lib/mongodb";
+import { messages } from "@/utils/messages";
+import User from "@/models/User";
+
+export async function GET(request: NextRequest) {
+  try {
+    await connectMongoDB();
+    const users = await User.find();
+
+    return NextResponse.json({ users }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: messages.error.default },
+      { status: 500 }
+    );
+  }
+}
